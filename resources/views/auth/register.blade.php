aaa6a9755ed9ea0fa8af23d0904c1c9c2e7006e9
Chumel25 <josemanueltoalamoreno131@gmail.com>
2026-01-12 13:56:41 -0600

    guardar cambios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,12 +65,6 @@
             font-weight: 600;
         }
 
-<<<<<<< HEAD
-        /* Inputss */
-        .barber-card input {
-            width: 100%;
-            padding: 12px;
-=======
         /* ERRORES */
         .error-box {
             background: #fee2e2;
@@ -78,7 +72,7 @@
             color: #991b1b;
             padding: 10px;
             border-radius: 6px;
->>>>>>> f4da8bc3d565cd1aa69a27bcafce1b9469b0e818
+             f4da8bc3d565cd1aa69a27bcafce1b9469b0e818
             margin-bottom: 15px;
             font-size: 14px;
         }
