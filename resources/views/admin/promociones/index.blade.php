5d0c635b32167912e9b82463f02d3c3bc5c35086
GuillenME <guillenmariana550@gmail.com>
2026-01-29 12:03:19 -0600

    arreglo del modulo de promociones

--- resources/views/admin/promociones/index.blade.php
+++ resources/views/admin/promociones/index.blade.php
@@ -9,55 +9,5 @@
 @endsection
 
 @section('content')
-
- 
-    <div class="card table-card">
-        <div class="table-container">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Descuento</th>
-                        <th>Servicios</th>
-                        <th>Fecha</th>
-                        <th>Publicada</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($promociones as $promo)
-                        <tr>
-                            <td>{{ $promo->title }}</td>
-                            <td>{{ $promo->discount }}%</td>
-                            <td>
-                                @foreach ($promo->servicios as $servicio)
-                                    <span class="badge-service">
-                                        {{ $servicio->name }}
-                                    </span>
-                                @endforeach
-                            </td>
-                            <td>
-                                {{ \Carbon\Carbon::parse($promo->start_date)->format('d/m/Y') }}
-                                -
-                                {{ \Carbon\Carbon::parse($promo->end_date)->format('d/m/Y') }}
-                            </td>
-                            <td>
-                                @if ($promo->published)
-                                    <span class="badge badge-on">Sí</span>
-                                @else
-                                    <span class="badge badge-off">No</span>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="{{ route('admin.promociones.edit', $promo) }}" class="btn-edit">
-                                    Editar
-                                </a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-
+    <livewire:admin.promocion-index />
 @endsection
