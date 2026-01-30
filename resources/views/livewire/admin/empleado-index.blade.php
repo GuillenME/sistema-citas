d8cb9492ffdc5baf18e2bf8f95462335238208a9
froilan <froilannajeramorales@gmail.com>
2026-01-30 10:57:51 -0600

    se integro cambios de hoy viernes 30 version 5

--- resources/views/livewire/admin/empleado-index.blade.php
+++ resources/views/livewire/admin/empleado-index.blade.php
@@ -1,40 +1,43 @@
-<div class="card table-container">
-    <table>
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Especialidad</th>
-                <th>Estado</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            @foreach($empleados as $e)
+<div>
+    <div class="card table-container">
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $e->name }}</td>
-                    <td>{{ $e->specialty }}</td>
-                    <td>
-                        @if($e->active)
-                            <span class="badge badge-on">Activo</span>
-                        @else
-                            <span class="badge badge-off">Inactivo</span>
-                        @endif
-                    </td>
-                    <td class="actions">
-                        <a href="{{ route('admin.empleados.edit', $e) }}" class="btn-edit">
-                            Editar
-                        </a>
-
-                        <button class="btn-delete" wire:click="toggle({{ $e->id }})">
-                            {{ $e->active ? 'Desactivar' : 'Activar' }}
-                        </button>
-                    </td>
+                    <th>Nombre</th>
+                    <th>Especialidad</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+
+            <tbody>
+                @foreach($empleados as $e)
+                    <tr>
+                        <td>{{ $e->name }}</td>
+                        <td>{{ $e->specialty }}</td>
+                        <td>
+                            @if($e->active)
+                                <span class="badge badge-on">Activo</span>
+                            @else
+                                <span class="badge badge-off">Inactivo</span>
+                            @endif
+                        </td>
+                        <td class="actions">
+                            <a href="{{ route('admin.empleados.edit', $e) }}" class="btn-edit">
+                                Editar
+                            </a>
+
+                            <button class="btn-delete" wire:click="toggle({{ $e->id }})">
+                                {{ $e->active ? 'Desactivar' : 'Activar' }}
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
     <div class="pagination-wrapper">
         {{ $empleados->links() }}
     </div>
+</div>
