d365f6a46096d46c5f79b05186c0ac59cb1683fa
Chumel25 <josemanueltoalamoreno131@gmail.com>
2026-02-06 10:31:19 -0600

    Cambio diseño del home con todo nuevo de la fecha 06 febrero version 1

--- app/Livewire/Contactos.php
+++ app/Livewire/Contactos.php
@@ -3,9 +3,44 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactoMail;
 
 class Contactos extends Component
 {
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $asunto;
+    public $mensaje;
+
+    protected $rules = [
+        'nombre'   => 'required|string|max:100',
+        'apellido' => 'nullable|string|max:100',
+        'email'    => 'required|email',
+        'asunto'   => 'nullable|string|max:150',
+        'mensaje'  => 'required|string|min:5',
+    ];
+
+    public function enviar()
+    {
+        $this->validate();
+
+        Mail::to(config('mail.from.address'))
+            ->send(new ContactoMail([
+                'nombre'   => $this->nombre,
+                'apellido' => $this->apellido,
+                'email'    => $this->email,
+                'asunto'   => $this->asunto,
+                'mensaje'  => $this->mensaje,
+            ]));
+
+        // limpiar formulario
+        $this->reset();
+
+        session()->flash('success', 'Mensaje enviado correctamente');
+    }
+
     public function render()
     {
         return view('livewire.contactos');
---
+++ app/Mail/ContactoMail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailable;
+
+class ContactoMail extends Mailable
+{
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this
+            ->subject('📩 Nuevo mensaje de contacto')
+            ->view('emails.contacto');
+    }
+}
