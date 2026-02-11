0e3a1138bd4e107624bbf7d7effe0cbeb6fa3734
GuillenME <guillenmariana550@gmail.com>
2026-02-11 10:33:47 -0600

    Cambie a 5 el maximo de especialidades en el empleado

--- app/Livewire/Admin/EmpleadoEdit.php
+++ app/Livewire/Admin/EmpleadoEdit.php
@@ -15,7 +15,7 @@ class EmpleadoEdit extends Component
     protected $rules = [
         'nombre' => 'required',
         'telefono' => 'required',
-        'serviciosSeleccionados' => 'required|array|min:1|max:3',
+        'serviciosSeleccionados' => 'required|array|min:1|max:5',
         'serviciosSeleccionados.*' => 'exists:services,id',
         'activo' => 'required|boolean'
     ];
@@ -31,8 +31,8 @@ class EmpleadoEdit extends Component
 
     public function updatedServiciosSeleccionados($value)
     {
-        if (count($this->serviciosSeleccionados) > 3) {
-            $this->serviciosSeleccionados = array_slice($this->serviciosSeleccionados, 0, 3);
+        if (count($this->serviciosSeleccionados) > 5) {
+            $this->serviciosSeleccionados = array_slice($this->serviciosSeleccionados, 0, 5);
         }
     }
 
--- resources/views/livewire/admin/promocion-edit.blade.php
+++ resources/views/livewire/admin/promocion-edit.blade.php
@@ -48,7 +48,7 @@
         <div class="form-group">
             <label>Servicios aplicables</label>
             {{-- 🔥 SIN defer para evitar bugs --}}
-            <select multiple wire:model="servicios">
+            <select multiple wire:model="servicios" class="promo-servicios-select">
                 @foreach($listaServicios as $servicio)
                     <option value="{{ $servicio->id }}">
                         {{ $servicio->name }}
