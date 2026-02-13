da515d7dfd667360ce7218f028344762209acb71
GuillenME <guillenmariana550@gmail.com>
2026-02-13 13:33:52 -0600

    Arreglo de la vista de editar empleado

--- app/Livewire/Admin/EmpleadoCreate.php
+++ app/Livewire/Admin/EmpleadoCreate.php
@@ -14,10 +14,20 @@ class EmpleadoCreate extends Component
 
     protected $rules = [
         'nombre' => 'required|min:3',
-        'telefono' => 'required',
+        'telefono' => 'required|digits:10',
         'serviciosSeleccionados' => 'required|array|min:1|max:5',
         'serviciosSeleccionados.*' => 'exists:services,id',
     ];
+
+    protected $messages = [
+        'telefono.required' => 'El telefono es obligatorio.',
+        'telefono.digits' => 'El telefono debe tener exactamente 10 digitos numericos.',
+    ];
+
+    public function updatedTelefono($value): void
+    {
+        $this->telefono = substr(preg_replace('/\D/', '', (string) $value), 0, 10);
+    }
 
     public function abrirConfirmacion()
     {
